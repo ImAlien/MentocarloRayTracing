923c950de8eb658f8ec24abab4de636611e9d82e
ImAlien <577068566@qq.com>
2022-03-21 23:28:29 +0800

    camera to world

--- montecarlo_ray_trace/scene/scene.cpp
+++ montecarlo_ray_trace/scene/scene.cpp
@@ -1,5 +1,6 @@
 
 #include"scene.h"
+#include"../test/Log.h"
 
 using namespace std;
 using namespace glm;
@@ -12,9 +13,50 @@ Scene::Scene(string name) {
 	obj = new Obj(pathname);
 	createBoundingBox();
 	this->bvh = new BVH(BBs);
+	this->df = new DataFrame(camera->width, camera->height);
+	shade();
+	df->inputImage();
+	LOG("保存图片");
+	camera->saveImage(df);
 }
 void Scene::createBoundingBox() {
 	for (Triangle* tr : obj->triangles) {
 		BBs.push_back(BoundingBox(tr));
 	}
+}
+void Scene::shade() {
+	LOG("开始渲染");
+	int w = camera->width, h = camera->height;
+	float theta = camera->fovy / 2;
+	
+	float zpos = 1.0 / tan(theta);
+	int idx = 0;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			// Screen space to NDC space
+			float nx = (i + 0.5f) * 2 / w - 1.0f;
+			float ny = (j + 0.5f) * 2 / h - 1.0f;
+			// NDC space to world space
+			vec3 c_dir = vec3(nx, ny, -zpos);
+			vec3 lookat_dir = normalize(camera->lookat - camera->eye);
+			vec3 up_dir = normalize(camera->up);
+			vec3 x_dir = normalize(cross(lookat_dir, up_dir));
+			vec3 world_dir = nx * x_dir + ny * lookat_dir - zpos * up_dir;
+			
+			Ray ray;
+			ray.direction = world_dir;
+			ray.startPoint = camera->eye;
+			
+			vec3 color = rayCasting(ray, 0);
+			df->data[idx++] = color.x;
+			df->data[idx++] = color.y;
+			df->data[idx++] = color.z;
+		}
+	}
+	LOG("渲染完成");
+}
+vec3 Scene::rayCasting(Ray& ray, int depth) {
+	if(bvh->intersectBVH(ray).isIntersect)
+		return vec3(1, 0, 0);
+	return vec3(0, 0, 0);
 }
--- montecarlo_ray_trace/scene/scene.h
+++ montecarlo_ray_trace/scene/scene.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include"../camera/Camera.h"
+#include"../camera/DataFrame.h"
 #include"../BVH/BoundingBox.h"
 #include"../BVH/BVH.h"
 #include "../obj/Obj.h"
 #include<vector>
 #include<string>
+#include"../ray/Ray.h"
 
 class Scene {
 public:
@@ -14,7 +16,10 @@ public:
 	std::vector<BoundingBox> BBs;
 	Obj* obj;
 	BVH* bvh;
+	DataFrame* df;
 	Scene(std::string name);
 	Scene();
 	void createBoundingBox();
+	void shade();
+	glm::vec3 rayCasting(Ray& ray, int depth);
 };
